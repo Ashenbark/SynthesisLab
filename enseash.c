057ab2143c0ede46e10f9ec1b2626d3d4e4e3fa6
Flopp88 <florianpp88@gmail.com>
2021-12-10 10:04:10 +0100

    q1

--- enseash.c
+++ enseash.c
@@ -1,11 +1,22 @@
 #include "enseash.h"
 
-static void consolePrint (const char *s)
-{
+static void consolePrint (const char *s){
     write(STDOUT_FILENO, s, strlen(s));
+}
+
+static char* consoleRead(){
+	char* reading = malloc(50);
+	read(STDIN_FILENO, reading, 50);
+	return reading;
 }
 
 
 int main(int argc ,char *argv [ ]){
-	consolePrint("test\n ");
+	
+	consolePrint("Welcome to ENSEA Tiny Shell.\nPour quitter, tapez 'exit'. \nensea \% ");
+	while(1){
+		consoleRead();
+		
+		consolePrint("ensea \% ");
+	}
 }
--- enseash.h
+++ enseash.h
@@ -1,6 +1,7 @@
 #include <unistd.h> 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static void consolePrint (const char *s);
-
+static char* consoleRead(void);
