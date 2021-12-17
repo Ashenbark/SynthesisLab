3d729554b9080f1bdb311958ac28c2048e357d90
Ashenbark <69201376+Ashenbark@users.noreply.github.com>
2021-12-17 12:09:47 +0100

    Add files via upload

--- enseash.c
+++ enseash.c
@@ -9,10 +9,11 @@ static char* consoleRead(){
 	char * reading = malloc(READSIZE);
 	int lenght = read(STDIN_FILENO, reading, READSIZE);
 	reading[lenght-1] = '\0';
+	
 	return reading;
 }
 	
-static int execute(char* command){
+static int execute(char* command, struct timespec * start, struct timespec * stop){
 	char* cmd_exit = "exit";
 	int status;
 	
@@ -20,31 +21,98 @@ static int execute(char* command){
 		consolePrint("Bye bye...\n");
 		exit(EXIT_SUCCESS);
 	}
-
+	
+	clock_gettime(CLOCK_REALTIME, start);
 	
 	pid_t pid = fork();
 	if (pid==0){
-		execlp(command, command, (char *) NULL);
+		int asOutput = 0;
+		int asInput = 0;
+		char * filename;
+		
+		char * token_array[COMMANDSIZE];
+
+		// We extract the first token
+		char * token = strtok(command, " ");
+
+		// Then loop through the string to extract all other tokens
+
+		int i = 0;
+		while( token != NULL ) {
+			if (asOutput == 1 || asInput == 1) {
+				filename = token;
+			} 
+			
+			token_array[i] = token;
+			
+			if (strcmp(token, ">") == 0) {
+			  asOutput = 1;
+			  token_array[i] = NULL;
+			}
+			if (strcmp(token, "<") == 0) {
+			  asInput = 1;
+			  token_array[i] = NULL;
+			}
+			
+			token = strtok(NULL, " ");
+			i++;
+		}
+		
+		// Finally we put a NULL as the last element of the array
+		token_array[i] = NULL;
+		
+		if (asOutput == 1) {
+			// reroot the output to a new file descriptor
+			mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+			int descriptor = creat(filename, mode);
+			dup2(descriptor, 1);
+		}
+		
+		execvp(token_array[0], token_array);
+	
+		consolePrint("Error: invalid command\n");
+	
 		exit(EXIT_SUCCESS);
 	}
+	
 	waitpid(pid, &status, 0);
-	return WEXITSTATUS(status);
-
+	clock_gettime(CLOCK_REALTIME, stop);
+	return status;
 } 
 	
-int main(int argc ,char *argv [ ]){
+void checkStatus(int status, struct timespec * start, struct timespec * stop){
+	char prompt[PROMPTSIZE];
+	double counter = (stop->tv_nsec - start->tv_nsec)/1e6;
+	
+	if (WIFEXITED(status)){
+		sprintf(prompt, "ensea [exit:%d|%.0lf ms] %% ", WEXITSTATUS(status), counter);
+	}
+	
+	else if (WIFSIGNALED(status)) {
+		sprintf(prompt, "ensea [sign:%d|%.0lf ms] %% ", WTERMSIG(status), counter);
+	}
+	
+	else {
+		sprintf(prompt,"ensea %% ");
+	}
+	
+	consolePrint(prompt);
+	return;
+}
+
+int main(){
+
 	char* command;
 	int ex_status;
-	char prompt[PROMPTSIZE];
+	struct timespec start, stop;
 	
 	consolePrint("Welcome to ENSEA Tiny Shell.\nPour quitter, tapez 'exit'\n");
 	consolePrint("ensea \% ");
 	
 	while(1){
 		command = consoleRead();
-		ex_status = execute(command);
-		sprintf(prompt, "ensea [exit:%d] %% ", ex_status);
-		consolePrint(prompt);
+		ex_status = execute(command, &start, &stop);
+		checkStatus(ex_status, &start, &stop);
 	}
 	
 	return 0;
--- enseash.h
+++ enseash.h
@@ -3,12 +3,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define READSIZE 50
 #define PROMPTSIZE 50
+#define COMMANDSIZE 20
 
 static void consolePrint (const char*);
 
 static char* consoleRead(void);
 
-static int execute(char*);
+static int execute(char*, struct timespec *, struct timespec *);
+
+void checkStatus(int, struct timespec *, struct timespec *);
